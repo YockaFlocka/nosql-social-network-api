edb3a47d316d36c359b4f41b5ef0282a275c66d4
Peter Yockey <peterjyockey@gmail.com>
2023-05-22 18:43:38 -0500

    added starter files

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const { User, Thought, Reaction } = require('../models');
+
+
+// ============  User Routes  ============== //
+
+
+// ============  Thought Routes  ============= //
+
+
+// ============  Reaction Routes  ============= //
+
+
+module.exports = router;
