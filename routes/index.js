1fdef0ceba845513f49746ccbf18909c1f8cd4eb
Peter Yockey <peterjyockey@gmail.com>
2023-05-24 17:42:42 -0500

    finished user routes

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const { User, Thought, Reaction } = require('../models');
 // Get all users
 router.get('/user', async (req, res) => {
     try {
-      const allUsers = await User.find()
+      const allUsers = await User.find();
       res.status(200).json({ status: "success", payload: allUsers });
     } catch (err) {
       res.status(400).json({ msg: err.message });
@@ -17,7 +17,7 @@ router.get('/user', async (req, res) => {
 // Get a user by id with thought and friends data
 router.get('/user/:id', async (req, res) => {
     try {
-        const user = await User.findOne({ _id: req.params.id }).populate({ path: "thoughts", path: "friends" })
+        const user = await User.findOne({ _id: req.params.id }).populate({ path: "thoughts", path: "friends" });
         res.status(200).json({ status: "success", payload: user });
       } catch (err) {
         res.status(400).json({ msg: err.message });
@@ -33,6 +33,31 @@ router.post('/user', async (req, res) => {
         res.status(500).json({ msg: err.message });
     }
 })
+
+// update user
+router.put('/user/:id', async (req, res) => {
+    try {
+      const updateUser = await User.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+      res.status(200).json({ status: "success", payload: updateUser });  
+    } catch (error) {
+      res.status(400).json({ msg: error.message });  
+    }
+})
+
+// delete user
+router.delete('/user/:id', async (req, res) => {
+    try {
+      const deleteUser = await User.findOneAndDelete({ _id: req.params.id });
+      res.status(200).json({ status: "success", message: "User has been deleted!" });  
+    } catch (error) {
+      res.status(400).json({ msg: error.message });  
+    }
+})
+
 
 // ============  Thought Routes  ============= //
 
